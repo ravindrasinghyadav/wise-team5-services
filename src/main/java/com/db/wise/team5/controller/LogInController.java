e96bf47cd9557ecb8aa25232a4a9b87eb02dfa0c
Ravindra Singh <ravindra.singh@gatech.edu>
2019-06-05 12:03:43 -0400

    Initial

--- src/main/java/com/db/wise/team5/controller/LogInController.java
+++ src/main/java/com/db/wise/team5/controller/LogInController.java
@@ -3,6 +3,6 @@ package com.db.wise.team5.controller;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class LoginController {
+public class LogInController {
 
 }
--- src/main/java/com/db/wise/team5/controller/SignUpController.java
+++ src/main/java/com/db/wise/team5/controller/SignUpController.java
@@ -3,6 +3,6 @@ package com.db.wise.team5.controller;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class SignupController {
+public class SignUpController {
 
 }
